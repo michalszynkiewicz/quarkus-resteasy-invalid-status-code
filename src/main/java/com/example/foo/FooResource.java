0a667d7ed94166c35a52d990d34a5ed4d22ce196
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2019-06-28 12:59:56 +0200

    changed post to accept text/plain

--- src/main/java/com/example/foo/FooResource.java
+++ src/main/java/com/example/foo/FooResource.java
@@ -1,8 +1,9 @@
 package com.example.foo;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,12 +14,16 @@ import javax.ws.rs.core.Response;
  * Date: 28/06/2019
  */
 @Path("/foos")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
 public class FooResource {
+   @GET
+   public Response foo() {
+      throw new IllegalStateException("forced exception");
+   }
 
    @POST
-   public Response foo() {
+   public Response bar() {
       throw new IllegalStateException("forced exception");
    }
 }
